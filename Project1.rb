7c4f73a37326a1d5f772685016c29350139ee18c
Ryan Cook <ryancook@Ryans-MacBook-Pro.local>
2020-03-10 14:00:47 +1100

    Second commit - added fucntionality for players to guess

--- Project1.rb
+++ Project1.rb
@@ -1,17 +1,41 @@
 player_names = []
-response = nil
+response = ""
 
-    while response != "N"
-        p "Hello there! Player enter your name:"
+    while response.downcase != "n"
+        p "Hello there! Potential victim, enter your name:"
         name = gets.chomp()
         player_names.push(name)
         p "Are there any more players? Please answer 'Y' or 'N'"
         response = gets.chomp()
-        if response == "Y".downcase
-        elsif response == "N".downcase #need to also add in where if the answer doesn't = Y or N
-        end
     end
+
 p player_names
+
+initial_guess = []
+guess = 100
+
+    for name in player_names
+        p "Dearest friend #{name}, now please choose a number between 0-3 (inclusive)"
+        guess = gets.chomp.to_i
+            while guess < 0 || guess > 3
+                p "That number doesn't appear to be between 0 and 3. Please choose a number between ZERO and THREE - inclusive"
+                p "-----------------------------------------------------------------------------------------------------------"
+                p "Please guess again"
+                guess = gets.chomp.to_i
+            end
+        initial_guess.push(guess)
+    end
+    p initial_guess
+
+total_correct_answer = initial_guess.inject(:+) #random method to add integers in array
+p total_correct_answer #confirmed that I gots me a new variable for the next bit
+
+
+
+
+
+
+
 
 
 # guess = nil
