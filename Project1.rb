c1c04588eddafe67d3f625f264efd9e2cd7616cf
Ryan Cook <ryancook@Ryans-MacBook-Pro.local>
2020-03-12 11:01:33 +1100

    removed some redundant code. Added the gem coloize for readability of users

--- Project1.rb
+++ Project1.rb
@@ -1,69 +1,61 @@
-require "tty-prompt" #gem number 1 - simple way to ask yes/no question
+require "tty-prompt"    #gem number 1 - simple way to ask yes/no question
 require "tty-box"
-require 'colorize'
+require "colorize"
 
 player_names = []
 response = ""
 prompt = TTY::Prompt.new
 
-print TTY::Box.frame "Welcome to *SPOOF!*\nSomeone's luck is about to run out T.T\nEveryone else, enjoy!"
-    while response != "\"No\"" #have to use the weird syntax here, because when I printed response that's how it was coming out from the gem
-        print TTY::Box.frame "Yo! Potential loser! Enter your name:"
-        name = gets.chomp()
-        player_names.push(name) # we're going to prompt for players names later on, so we're pushing them into an array for later on
-        response = prompt.select("Are there any more players?", %w("Yes" "No")) #purpose of this is to know when to exit the loop
-    end
-
-
+print TTY::Box.frame "Welcome to * SPOOF! *\nSomeone's luck is about to run out T.T\nEveryone else, enjoy!"
+    
+while response != "\"No\"" #have to use the weird syntax here, because when I printed response that's how it was coming out from the gem
+    print TTY::Box.frame "If you're prepared to take the risk,\nplease enter your name:"
+    name = gets.chomp()
+    player_names.push(name) # we're going to prompt for players names later on, so we're pushing them into an array for later on
+    response = prompt.select("Are there any more players?", %w("Yes" "No")) #purpose of this is to know when to exit the loop
+end
 
 def spoof(player_names)
     
     exit_loop = 0
     guess = 100
+    i = 0
     initial_guess = []
 
-    print TTY::Box.frame "So, the potential losers for those game are:", player_names
+    print TTY::Box.frame "The potential losers for those game are:", player_names
 
     for name in player_names
         print TTY::Box.frame "#{name}, now please choose a number between 0-3 (inclusive)"
         guess = gets.chomp.to_i
             while guess < 0 || guess > 3
-                print TTY::Box.frame "That number doesn't appear to be between 0 and 3. Please choose a number between ZERO and THREE - inclusive. \nPlease guess again".colorize(:red)
+                print TTY::Box.frame "#{name}, it seems like you have some difficulty following instructions.\nThat number doesn't appear to be between 0 and 3.\nPlease choose a number between ZERO (0) and THREE (3) - inclusive.\nPlease guess again.".colorize(:red)
                 guess = gets.chomp.to_i
             end
         initial_guess.push(guess)
     end
-    
-# (p initial_guess) this needs to be hidden because we don't want people to know what the answer is
 
 total_correct_answer = initial_guess.inject(:+)
 
-# p total_correct_answer this needs to be hidden because we don't want people to know what the answer is
-
-i = 0
-# in reality, the person who went 2nd the first time would then go first, and the person who went first could go last etc
-# need to get it so that after we've been through once, we ask for input again
     while i < player_names.length
-        print TTY::Box.frame "#{player_names[i]}, here's where things start to get interesting. Ready to win?"
+        print TTY::Box.frame "#{player_names[i]}, here's where things start to get interesting.\nReady to win?"
         player_names.empty? == false
         print TTY::Box.frame "Please place your guess for the TOTAL amount"
             final_guess = gets.chomp.to_i
             if final_guess == total_correct_answer
                 player_names.delete(player_names[i])
-                print TTY::Box.frame "Winner winner chicken dinner!!", "Now that you've won, you may leave the game!"
+                print TTY::Box.frame "Winner winner chicken dinner!!\nNow that you've won, you may leave the game!".colorize(:green)
                 return
             elsif player_names.length >= 2 && i == player_names.length - 1
-                i = 0
-                print TTY::Box.frame "NOPE"
-                exit_loop +=1
-                p exit_loop
+                print TTY::Box.frame "NOPE!".colorize(:red)
+                exit_loop = exit_loop + 1
             else
-                print TTY::Box.frame "NOPE"
-                exit_loop +=1
+                print TTY::Box.frame "NOPE!".colorize(:red)
+                exit_loop = exit_loop + 1
                 i = i + 1
-            end 
+            end   
         if exit_loop == player_names.length
-            break
+        print TTY::Box.frame "Since no one was able to get the correct answer,\nlet's try again!"
+        break
         end
     end 
 end
@@ -74,11 +66,4 @@ def runner(player_names)
     end
 end
 
-### ideally, if everyone gets the answer wrong, it would just loop again straight away
-
 runner(player_names)
-    #now that someone's run, we want to run the guessing part of the program MINUS that person
-    # from the array
-    #wrap function call (player_names) until array == array.length == 1
-
-    #force select gems
